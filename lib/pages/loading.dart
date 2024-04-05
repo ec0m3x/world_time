b8ab595d59e2cb83da372ca8a78b4b1b89d3fec6
ecomex <skoch@sks-concept.de>
2024-04-05 15:45:59 +0200

    init

---
+++ lib/pages/loading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+  
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
+    );
+  }
+}
